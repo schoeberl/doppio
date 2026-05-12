bcc52276762adc2284bac162dfc700836989f1bf
Martin Schoeberl <martin@jopdesign.com>
2026-05-12 14:45:02 +0200

    Java wrapper

--- src/main/scala/examples/Accumulator.scala
+++ src/main/scala/examples/Accumulator.scala
@@ -2,13 +2,15 @@ package examples
 
 import chisel3._
 
+class AccumulatorIO(width: Int) extends Bundle {
+  val in  = Input(UInt(width.W))
+  val out = Output(UInt(width.W))
+  val en  = Input(Bool())
+  val rst = Input(Bool())
+}
+
 class Accumulator(width: Int) extends Module {
-  val io = IO(new Bundle {
-    val in  = Input(UInt(width.W))
-    val out = Output(UInt(width.W))
-    val en  = Input(Bool())
-    val rst = Input(Bool())
-  })
+  val io = IO(new AccumulatorIO(width))
 
   val sum = RegInit(0.U(width.W))
 
